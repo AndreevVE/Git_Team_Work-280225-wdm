a8463fde106ccfd3b1748c06de62f9f517ff1a0a
Anastasiia <sulollarianastasiia@gmail.com>
2025-06-03 14:26:17 +0200

    Автоматический коммит 2025-06-03 14:26:17

--- git_commit_anastasiia.sh
+++ git_commit_anastasiia.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 if git status > /dev/null 2>&1; then
-  echo "Это git-репозиторий"
+   echo "Это git-репозиторий"
 
-  git add -A
+   git add -A
+if git commit -m "Автоматический коммит $(date +'%Y-%m-%d %H:%M:%S')" > /dev/null 2>&1; then
+   echo " Изменения успешно зафиксированы."
+   exit 0
+else
+   echo "️ Нет изменений для коммита."
+   exit 0
+fi
 
-  if git commit -m "Автоматический коммит $(date +'%Y-%m-%d %H:%M:%S')" > /dev/null 2>&1; then
-    echo " Изменения успешно зафиксированы."
- exit 0
-    else
-    echo "️ Нет изменений для коммита."
- exit 0
-  fi
-
-   else
-     echo "Не git репозиторий"
-  exit 1
+else
+   echo "Не git репозиторий"
+   exit 1
 fi
