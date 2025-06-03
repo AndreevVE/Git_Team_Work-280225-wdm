cd1dd16ddc0fbb712db9d14d0e80d8a3180d9159
Olga Bibikova <o.bibikova22@gmail.com>
2025-06-03 10:25:59 +0200

    Finished script for task 5

--- filemover.sh
+++ filemover.sh
@@ -10,6 +10,27 @@
 
 
 # Проверка, есть ли файлы с указанным расширением в исходной директории
+#!/bin/bash
+SOURCE_DIR='sourse_directory'
+EXTENSION='file_extension'
+
+found=0
+for file in "$SOURCE_DIR"/*."$EXTENTION"; do
+   if [ -e "$file"]; then
+     found=1
+     break
+   fi
+done
+
+   if [ "$found" -eq 0 ]; then
+  echo "There are no files with this extention: .$EXTENTION"
+  exit 1
+
+   else 
+  echo "Files with .$EXTENTION were found"
+
+   fi  
+
 
 # Копирование файлов с указанным расширением в целевую директорию
 
