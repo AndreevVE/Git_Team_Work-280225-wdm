2443911370e5fac65ea5f87692f130ee9f88336b
KristinaSchmidt <kristina_schmidt21@web.de>
2025-06-03 11:25:53 +0200

    New File

--- filemover.sh
+++ filemover.sh
@@ -12,6 +12,17 @@
 # Проверка, есть ли файлы с указанным расширением в исходной директории
 
 # Копирование файлов с указанным расширением в целевую директорию
+echo "Копирование и переименование файлов:"
 
+for file in "$source_directory"/*."$file_extension"
+do
+  filename=$(basename "$file")
+  name="${filename%.*}"
+  new_filename="$name.$new_file_extension"
+  cp "$file" "$target_directory/$new_filename"
+
+  echo "Скопирован: $filename → $new_filename"
+
+done
 # Архивация исходных файлов.
 
